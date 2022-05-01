af8d5502d3ecd1ab5b607151cd1ac85534b9d430
agguuaa <100287322+agguuaa@users.noreply.github.com>
2022-05-01 10:32:14 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,70 @@
+//Bonus
+function showPosition(position) {
+  let currentLocation = document.querySelector("currentPosition");
+  currentLocation.innerHTML = `Your latitude is ${position.coords.longtitude} and your longtitude is ${position.coords.longtitude}`;
+}
+
+function getCurrentPosition() {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(showPosition);
+}
+let button = document.querySelector("#currentPosition");
+button.addEventListener("click", getCurrentPosition);
+
+//Search city and display city name & current temp
+function searchEngine(city) {
+  let apiKey = "af683468eb6c609597efb70857e6314f";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+  axios.get(`${apiUrl}&appid=${apiKey}`).then(showTemp);
+}
+
+//Show city name
+function showCity(event) {
+  event.preventDefault();
+  let city = document.querySelector("#searchQueryInput");
+  let cityName = document.querySelector("#current-city");
+  event.preventDefault();
+  cityName.innerHTML = `${city.value}`;
+  searchEngine(city.value);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", showCity);
+
+//Show temperature
+function showTemp(response) {
+  let temp = Math.round(response.data.main.temp);
+  let degrees = document.querySelector("#current-temperature");
+  degrees.innerHTML = `${temp}`;
+  let currentCity = document.querySelector("#current-city");
+  currentCity.innerHTML = response.data.name;
+}
+
+let now = new Date();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday"
+];
+
+let hours = now.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = now.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+
+let daySpace = document.querySelector("#currentDay");
+let day = days[now.getDay()];
+
+let timeSpace = document.querySelector("#currentTime");
+
+daySpace.innerHTML = `${day}`;
+timeSpace.innerHTML = `${hours}:${minutes}`;
