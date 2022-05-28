1cca3ce9ce426b39dbb4185613113bddecf9629e
agguuaa <100287322+agguuaa@users.noreply.github.com>
2022-05-28 19:23:41 +0200

    update search engine

--- src/index.js
+++ src/index.js
@@ -1,50 +1,3 @@
-let now = new Date();
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-
-let hours = now.getHours();
-if (hours < 10) {
-  hours = `0${hours}`;
-}
-let minutes = now.getMinutes();
-if (minutes < 10) {
-  minutes = `0${minutes}`;
-}
-
-let daySpace = document.querySelector("#daySpace");
-let day = days[now.getDay()];
-
-let timeSpace = document.querySelector("#timeSpace");
-
-daySpace.innerHTML = `${day}`;
-timeSpace.innerHTML = `${hours}:${minutes}`;
-
-//
-function showPosition(position) {
-  let city = document.querySelector("currentPosition");
-  city.innerHTML = `Your latitude is ${position.coords.longtitude} and your longtitude is ${position.coords.longtitude}`;
-}
-
-function getForecast(coordinates) {
-  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayForecast);
-}
-
-function getCurrentPosition() {
-  event.preventDefault();
-  navigator.geolocation.getCurrentPosition(showPosition);
-}
-let button = document.querySelector("#currentPosition");
-button.addEventListener("click", getCurrentPosition);
-
 //Search city and display city name & current temp
 function searchEngine(city) {
   let apiKey = "af683468eb6c609597efb70857e6314f";
@@ -70,11 +23,45 @@ form.addEventListener("submit", showCity);
 function showTemp(response) {
   let temp = Math.round(response.data.main.temp);
   let degrees = document.querySelector("#current-temperature");
-  degrees.innerHTML = `${temp} °C|°F`;
+  degrees.innerHTML = `${temp}`;
   let currentCity = document.querySelector("#current-city");
   currentCity.innerHTML = response.data.name;
-  iconElement.setAttribute(
-    "src",
-    `http://openweathermap.org/img/wn/10d@2x.png`
-  );
+}
+
+function tempCF(response) {
+  let;
+}
+
+function formatDate(timestamp) {
+  let now = new Date(timestamp);
+
+  let hours = now.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = now.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = days[Date.getDay()];
+  return `${day} ${getHours}:${getMinutes}`;
+
+  function formatDay(timestamp) {
+    let date = new Date(timestamp * 1000);
+    let day = date.getDay();
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+    return days[day];
+  }
 }
