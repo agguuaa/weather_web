4f211dbc133bf9f4d49ae5c487a66283671ea73e
agguuaa <100287322+agguuaa@users.noreply.github.com>
2022-05-31 19:16:55 +0200

    Updated iconElement

--- src/index.js
+++ src/index.js
@@ -15,8 +15,13 @@ function showTemp(response) {
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
-  iconElement.setAttribute("src", `https://openweathermap.org/img/wn/01d.png`);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 function searchEngine(city) {
